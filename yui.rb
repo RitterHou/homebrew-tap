bc7b7daec8c6e1d0aaccd0288394211253b315c0
侯锐 <hourui@qianmi.com>
2018-11-29 10:24:31 +0800

    新增yui的安装脚本

---
+++ yui.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Yui < Formula
+  desc "用go语言实现四则运算"
+  homepage "https://github.com/RitterHou/yui"
+  url "https://github.com/RitterHou/yui/releases/download/0.0.6/yui_drawin"
+  sha256 "c6250259c4df8536b777781a55f8fbe2b6dd59bb9b6d7bb62a261516fd034e1d"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "yui_drawin"
+    mv bin/"yui_drawin", bin/"yui"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test yui`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
